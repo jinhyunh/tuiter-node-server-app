6920cf83e1d62b3a06c38d177fb0af6d18f805eb
Jinhyun Hwang <jinhyunh0808@gmail.com>
2023-08-08 13:36:12 -0400

    Added changes that i think will fix the netlify

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ import AuthController from "./users/auth-controller.js";
 import session from "express-session";
 
 const app = express();
-app.use(cors({
-  credentials: true,
-  origin: "http://localhost:3000",
-}
-));
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
 const sessionOptions = {
     secret: "any string",
     resave: false,
